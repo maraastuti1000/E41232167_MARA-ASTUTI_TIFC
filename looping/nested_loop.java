57bbcade057f977e012fcbb582ad99fd6c69b862
maraastuti1000 <158534399+maraastuti1000@users.noreply.github.com>
2024-02-04 10:50:59 +0700

    Create nested_loop.java

---
+++ looping/nested_loop.java
@@ -0,0 +1,22 @@
+package looping;
+/**
+ *
+ * @author 88697
+ */
+import java.util.Scanner;
+public class nested_loop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { for(int x = 0; x < 5; x++){
+        for (int y = 5; y < 10; y++)
+            System.out.print("x"+x + "-y" + y +"");
+        System.out.println();
+        System.out.println("testes yes yes yes");
+        System.out.println("yayyyyyyyyyyyyyyyy");
+    }
+        
+    }
+    
+}
