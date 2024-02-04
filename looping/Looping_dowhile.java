4d46874f0182c625a142a70deecbb6dff67340f8
maraastuti1000 <158534399+maraastuti1000@users.noreply.github.com>
2024-02-04 10:43:00 +0700

    Create Looping_dowhile.java

---
+++ looping/Looping_dowhile.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package looping;
+
+/**
+ *
+ * @author 88697
+ */
+import java.util.Scanner;
+public class Looping_dowhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int count = 10;
+        do {
+            System.out.println("Hai");
+            count--;
+        }   while (count > 1);
+        
+    }
+    
+}
